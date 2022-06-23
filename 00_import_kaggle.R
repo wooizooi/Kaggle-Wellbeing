4410ac859dda78ed9659d58b3d8e1b1e0896ed4d
Konstantin Roethke <57216333+wooizooi@users.noreply.github.com>
2022-06-23 16:12:26 +0200

    Add files via upload

---
+++ 00_import_kaggle.R
@@ -0,0 +1,4 @@
+#https://www.kaggle.com/ydalat/lifestyle-and-wellbeing-data
+#http://www.authentic-happiness.com/your-life-satisfaction-score
+library(readxl)
+KA <- read_excel("Desktop/Kaggle Wellbeing/Wellbeing_and_lifestyle_data_Kaggle.xlsx")
