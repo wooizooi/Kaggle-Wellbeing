ff41729e340ebd7ab2c3c41457f872c2199710d6
Konstantin Roethke <57216333+wooizooi@users.noreply.github.com>
2022-06-23 16:12:09 +0200

    Delete 00_import_kaggle.R

--- 00_import_kaggle.R
+++
@@ -1,8 +0,0 @@
-#https://www.kaggle.com/ydalat/lifestyle-and-wellbeing-data
-#http://www.authentic-happiness.com/your-life-satisfaction-score
-# library(readr)
-# KA <- read.csv("C:/Users/Roethke/Google Drive/RESEARCH/PROJEKTE/IS Well-Being/fallback data/Well-being/Kaggle/Wellbeing_and_lifestyle_data_Kaggle.csv",sep = "")
-# library(readxl)
-# KA <- read_excel("C:/Users/Roethke/Google Drive/RESEARCH/PROJEKTE/IS Well-Being/fallback data/Well-being/Kaggle/Wellbeing_and_lifestyle_data_Kaggle.csv.xlsx")
-library(readr)
-KA <- read_csv("C:/Users/Roethke/Google Drive/RESEARCH/PROJEKTE/IS Well-Being/fallback data/Well-being/Kaggle/wellbeing.csv")
